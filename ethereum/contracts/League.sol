5d67860f4dbfcce148ded4dbed442b81d84fab2d
Matt Fewer <mattfewer@hotmail.com>
2018-05-31 22:43:11 +0200

    updated League contract and added LeagueFactory

--- ethereum/contracts/League.sol
+++ ethereum/contracts/League.sol
@@ -1,25 +1,57 @@
 pragma solidity ^0.4.17;
+/* idea: implement way for teams to veto or approve trades or leage votes */
+
+contract LeagueFactory {
+    address[] public deployedLeagues;
+
+    function createLeague(uint entryFee, uint size, uint firstPot, uint secondPot, uint thirdPot) {
+        address newLeague = new League(msg.sender, uint entryFee, uint size, uint firstPot, uint secondPot, uint thirdPot);
+        deployedLeagues.push(newLeague);
+    }
+
+    function getDeployedLeagues() public view returns (address[]) {
+        return deployedLeagues;
+    }
+}
 
 contract League {
-    address[] public teams;
+    mapping(address => bool) public teams;
     address public manager;
+
     uint public buyInAmount;
     uint public leagueSize;
+    uint teamCount;
 
+    uint public firstPlacePot;
+    uint public secondPlacePot;
+    uint public thirdPlacePot;
 
-    function League(address creator, uint entryFee, uint size) public {
+    constructor(address creator, uint entryFee, uint size, uint firstPot, uint secondPot, uint thirdPot) public {
         manager = creator;
         buyInAmount = entryFee;
         leagueSize = size;
+
+        firstPlacePot = firstPot;
+        secondPlacePot = secondPot;
+        thirdPlacePot = thirdPot;
     }
 
     function enterLeague() public payable {
         require(msg.value == buyInAmount);
-        teams.push(msg.sender);
+        require(!teams[msg.sender]);
+
+        // adds a new key to the mapping equal to msg.sender and set val to true
+        // a mapping is better because the search time is constant for the entire mapping.
+        teams[msg.sender] = true;
+        teamCount++;
     }
 
-    function getTeams() public view returns (address[]){
-        return teams;
+    function pickWinners(address firstPlaceAddr, address secondPlaceAddr, address thirdPlaceAddr) public restricted payable {
+        require(teams[firstPlaceAddr] && teams[secondPlaceAddr] && teams[thirdPlaceAddr]);
+
+        firstPlaceAddr.transfer(firstPlacePot);
+        secondPlaceAddr.transfer(secondPlacePot);
+        thirdPlaceAddr.transfer(thirdPlacePot);
     }
 
     modifier restricted() {
