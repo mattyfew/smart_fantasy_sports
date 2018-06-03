525a2feb7d8c70d13b3960a84445acb2b2d9873a
Matt Fewer <mattfewer@hotmail.com>
2018-06-03 23:05:55 +0200

    first test passed woohoo 🛠

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -12,8 +12,8 @@ const output = solc.compile(source, 1).contracts;
 fs.ensureDirSync(buildPath);
 
 for (let contract in output) {
-  fs.outputJsonSync(
-    path.resolve(buildPath, contract.replace(':', '') + '.json'),
-    output[contract]
-  );
+    fs.outputJsonSync(
+        path.resolve(buildPath,
+        contract.replace(':', '') + '.json'), output[contract]
+    )
 }
---
+++ test/League.test.js
@@ -0,0 +1,43 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+
+const compiledFactory = require('../ethereum/build/LeagueFactory.json')
+const compiledLeague = require('../ethereum/build/League.json')
+
+let accounts
+let factory
+let leagueAddress
+let league
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+
+    // this deploys a contract, hence deploy(). we do not provide an address
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    await factory.methods.createLeague('1', '12', '9', '2', '1').send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    [ leagueAddress ] = await factory.methods.getDeployedLeagues().call()
+    // this accesses the ABI for this contract at an address that we specify
+    // ABI is first arg, address is second
+    league = await new web3.eth.Contract(
+        JSON.parse(compiledLeague.interface),
+        leagueAddress
+    )
+
+
+})
+
+describe('Leagues', () => {
+    it('deploys a factory and a league', () => {
+        assert.ok(factory.options.address)
+        assert.ok(league.options.address)
+    })
+})
